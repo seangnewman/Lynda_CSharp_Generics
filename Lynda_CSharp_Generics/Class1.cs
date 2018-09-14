5d0d41449405fce2920d78c10e57c41d6bc5f932
Sean Newman <seangnewman@msn.com>
2018-09-14 06:15:49 -0700

    Add project files.

---
+++ Lynda_CSharp_Generics/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lynda_CSharp_Generics
+{
+    public class Class1
+    {
+    }
+}
